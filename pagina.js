f3d6dade39a57d230e547398aca883d65efa8459
soares <gysoares99@gmail.com>
2026-07-01 16:14:05 -0300

    Pseudocodigo JS. Adição das fotos para teste Passei na IA pela segunda vez para arrumar alguns problemas. OBS: usei IA, google, youtube e documentos para achar muitos comando que eu não sabia, mas a logica é minha.

---
+++ pagina.js
@@ -0,0 +1,8 @@
+// Função para rodar o carrossel
+    //Pega as 3 fotos do retangulo especifico, coloca no array
+    //troca a logica de posição
+
+
+
+//addEventListener observa se tem um evendo de click do mause ou um espaço, ou uma setinha para o lado
+    //quando ocorrer, ativa a função
